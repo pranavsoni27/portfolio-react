1319cbef3bd44bee6b0691d83c7345258dcfa4cd
pranavsoni27 <pranavsoni2702@gmail.com>
2025-01-31 06:09:19 +0530

    Fixed asset loading and script initialization issues

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-react/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[ext]'
+        manualChunks: undefined
       }
     }
   }
